2cd5af6b302915e19f8ebb77876fef1e2959ff8d
Mandy Anne <picaq3.14@gmail.com>
2020-05-25 18:50:36 -0700

    made labels clickable

--- sarasa/sarasa.js
+++ sarasa/sarasa.js
@@ -7,8 +7,18 @@ let east = document.getElementsByClassName('east');
 let all = [...west, ...east];
 
 function change() {
-    let weight =  document.getElementById("weight").value;
+    let weight = document.getElementById("weight").value;
     for ( i = 0; i < all.length; i++ ) {
       all[i].style.fontWeight = weight;
     }
+}
+
+function xLight() {
+    weight.value = 200;
+    change();
+}
+
+function bold() {
+    weight.value = 600;
+    change();
 }
