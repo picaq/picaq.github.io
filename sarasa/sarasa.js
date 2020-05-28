24212c4bb2879be90662c83447499fd697113b57
Mandy Anne <picaq3.14@gmail.com>
2020-05-27 18:01:22 -0700

    sticky incompatible with ancestor overflow: hidden

--- sarasa/sarasa.js
+++ sarasa/sarasa.js
@@ -22,3 +22,27 @@ function bold() {
     weight.value = 600;
     change();
 }
+
+
+
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the header
+// var glyphs = document.getElementById("glyphs");
+// var characters = document.getElementById("characters");
+// var bq0 = document.getElementsByTagName("blockquote")[0];
+
+// // Get the offset position of the navbar
+// // var sticky = glyphs.offsetTop;
+// var sticky = glyphs.offsetParent.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     glyphs.classList.add("sticky");
+//   } else {
+//     glyphs.classList.remove("sticky");
+//   }
+// }
