5808f0d327a80721dc03d2a9d0d517a97a25c530
Mandy Anne <picaq3.14@gmail.com>
2020-05-28 02:40:47 -0700

    fixed check boxes with classes mods instead of css attribute mods

--- sarasa/sarasa.js
+++ sarasa/sarasa.js
@@ -1,7 +1,12 @@
 // window.onload = change();
 
-let west = document.getElementsByClassName('west');
-let east = document.getElementsByClassName('east');
+// document.querySelectorAll('.west')
+
+// let west = document.getElementsByClassName('west');
+// let east = document.getElementsByClassName('east');
+
+let west = document.querySelectorAll('.west');
+let east = document.querySelectorAll('.east');
 
 // let all = [].concat(west, east);
 let all = [...west, ...east];
@@ -38,4 +43,42 @@ function halfRem() {
 function fiveRem() {
     size.value = 5;
     changer();
+}
+
+// let italicStore = false;
+// let slabStore = false;
+
+function italicize() {
+    let italic = document.getElementById("italic");
+    if (italic.checked === true) {
+        for ( i = 0; i < all.length; i++ ) {
+        // all[i].style.fontStyle = "italic";
+        all[i].classList.add('italic');
+        }
+        // italicStore = true;
+    }
+    else if (italic.checked === false) {
+        for ( i = 0; i < all.length; i++ ) {
+        // all[i].style.fontStyle = "regular";
+        all[i].classList.remove('italic');
+        }
+        // italicStore = false;
+    }
+}
+
+function slabify() {
+    let family = document.getElementById("slab");
+    if (family.checked === true) {
+        for ( i = 0; i < west.length; i++ ) {
+            // west[i].style.fontFamily = 'Sarasa Mono Slab K';
+            west[i].classList.add('slab');
+        }
+    }
+    else if (family.checked !== true) {
+        for ( i = 0; i < west.length; i++ ) {
+        // west[i].style.fontFamily = 'Sarasa Mono Slab J';
+        west[i].classList.remove('slab');
+        }    
+    
+    }
 }
