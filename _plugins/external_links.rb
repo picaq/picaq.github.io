10695c792f20c6cef7bb1c8cfb0c3077cdd1e471
Mandy Chen <mandychen.art@gmail.com>
2025-05-09 17:26:02 -0700

    ensure unicode chars are nfc normalized

--- _plugins/external_links.rb
+++ _plugins/external_links.rb
@@ -9,3 +9,11 @@ Jekyll::Hooks.register :pages, :post_render do |page|
     page.output.gsub!(%r{<a\s+(?![^>]*target=)([^>]*href="https?://[^"]+")(.*?)>}, '<a \1 target="_blank" \2>')
   end
 end
+
+
+Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
+  next unless doc.output_ext == '.html'
+
+  normalized_output = add_first_letter_class(doc.output)
+  doc.output = normalized_output.unicode_normalize(:nfc)
+end
